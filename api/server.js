7b7ac28476f6c20f63ecf1c404b56155469278f9
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-24 13:47:53 -0700

    node server up and running

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Welcome to the backend server 🤘🏼💀✨'
+    })
+})
+
+module.exports = server;
