1bf0a4270d6368d0c683ddddb54075bf0fc575fe
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-25 08:39:14 -0700

    users model built out

---
+++ models/users-model.js
@@ -0,0 +1,35 @@
+const db = require('../dbConfig.js');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById,
+    findByEmail
+};
+
+function find() {
+    return db('users').select('id', 'username', 'password', 'department');
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
+}
+
+async function add (user) {
+    const [id] = await db('users').insert(user);
+
+    return findById(id);
+}
+
+function findById(id) {
+    return db('users')
+    .where({ id })
+    .first();
+}
+
+function findByEmail(email) {
+    db('users')
+    .where({ email })
+    .first();
+}
